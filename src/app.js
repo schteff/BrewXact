7b1034d58cd3ec6ce4bbca3a9421e1bed388e307
Stefan <stefan@tempusinfo.se>
2020-01-17 20:17:51 +0100

    work in progress

--- src/app.js
+++ src/app.js
@@ -1,18 +1,29 @@
 const http = require("http");
 const sensor = require("ds18b20-raspi");
+const fs = require("fs");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  const temps = sensor.readAllC();
-  console.log(temps);
+fs.readFile("./index.html", function(err, html) {
+  if (err) {
+    throw err;
+  }
+  http
+    .createServer(function(request, response) {
+      console.log(request);
+      response.writeHeader(200, { "Content-Type": "text/html" });
+      response.write(html);
+      response.end();
 
-  res.end(JSON.stringify(temps));
-});
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "application/json");
+      const temps = sensor.readAllC();
+      console.log(temps);
 
-server.listen(port, hostname, () => {
-  console.log(`Temp server running at http://${hostname}:${port}/`);
+      response.end(JSON.stringify(temps));
+    })
+    .listen(port, hostname, () => {
+      console.log(`Temp server running at http://${hostname}:${port}/`);
+    });
 });
