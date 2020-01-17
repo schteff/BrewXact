2d4a884bef275a9149618bccd009abc6cd165fca
Stefan <stefan@tempusinfo.se>
2020-01-17 20:11:45 +0100

    just serve the temp object as json

--- src/app.js
+++ src/app.js
@@ -6,13 +6,13 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Content-Type", "application/json");
   const temps = sensor.readAllC();
   console.log(temps);
 
-  res.end("Hello World " + temps);
+  res.end(JSON.stringify(temps));
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Temp server running at http://${hostname}:${port}/`);
 });
