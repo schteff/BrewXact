854445d7f6509dda56308513365d6c131020784e
Stefan <schteff@gmail.com>
2020-01-18 16:15:43 +0100

    major

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const sensor = require("ds18b20-raspi");
+const jsonfile = require("jsonfile");
 
 const config = require("./config/config.js"), // import config variables
   port = config.port, // set the port
@@ -14,8 +15,35 @@ var server = app.listen(port, function() {
 });
 
 // Use '/' to go to index.html via static middleware
+const file = "../data.json";
+var fileArray = [];
+jsonfile.readFile(file, function(err, obj) {
+  if (err) {
+    fileArray.push({ time: new Date().getTime(), temps: sensor.readAllC() });
+    jsonfile.writeFile(file, fileArray, function(err2) {
+      if (err2) console.error(err2);
+    });
+  } else {
+    fileArray = fileArray.concat(jsonfile.readFileSync(file));
+  }
+});
 
 app.get("/temps", function(req, res) {
   const temps = sensor.readAllC();
+  fileArray.push({ time: new Date().getTime(), temps: temps });
+  jsonfile.writeFile(file, fileArray, function(err) {
+    if (err) console.error(err);
+  });
+
   res.send(JSON.stringify(temps));
+});
+
+app.get("/init", function(req, res) {
+  const temps = sensor.readAllC();
+  fileArray.push({ time: new Date().getTime(), temps: temps });
+  jsonfile.writeFile(file, fileArray, function(err) {
+    if (err) console.error(err);
+  });
+
+  res.send(JSON.stringify(fileArray));
 });
